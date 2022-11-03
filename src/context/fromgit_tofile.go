17a5aab9f43bd736ca15bed61c36ef47948794d2
kengo-k <kurobanekengo@gmail.com>
2022-11-03 18:55:07 +0900

    context実装

--- src/context/fromgit_tofile.go
+++ src/context/fromgit_tofile.go
@@ -3,15 +3,23 @@
 
 package context
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/kengo-k/password-manager/loader"
+)
 
 func init() {
 	fmt.Println("mode: git2file")
 }
 
 func Load() ([]string, error) {
-	fmt.Println("load from git")
-	return nil, nil
+	g := &loader.GitLoader{}
+	passwords, err := g.Load()
+	if err != nil {
+		return nil, err
+	}
+	return passwords, nil
 }
 
 func Save() error {
