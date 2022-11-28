d41c59ac94694731276a9d503d570520074f4b24
kengo-k <kurobanekengo@gmail.com>
2022-11-28 21:28:58 +0900

    #7: password更新処理の実装

---
+++ src/model/password_test.go
@@ -0,0 +1,46 @@
+package model
+
+import (
+	"testing"
+)
+
+func getStrPointer(s string) *string {
+	return &s
+}
+
+func TestApplyUpdateValues(t *testing.T) {
+	pwd := Password{
+		Name:     "name",
+		Desc:     getStrPointer("desc"),
+		User:     getStrPointer("user"),
+		Password: getStrPointer("password"),
+		Mail:     getStrPointer("mail"),
+		Note:     getStrPointer("note"),
+	}
+	pwd.ApplyUpdateValues(&PasswordUpdateRequest{
+		Name:     getStrPointer("new name"),
+		Desc:     getStrPointer("new desc"),
+		User:     getStrPointer("new user"),
+		Password: getStrPointer("new password"),
+		Mail:     getStrPointer("new mail"),
+		Note:     getStrPointer("new note"),
+	})
+	if pwd.Name != "new name" {
+		t.Errorf("got: %v, expected: %v", pwd.Name, "new name")
+	}
+	if *pwd.Desc != "new desc" {
+		t.Errorf("got: %v, expected: %v", *pwd.Desc, "new desc")
+	}
+	if *pwd.User != "new user" {
+		t.Errorf("got: %v, expected: %v", *pwd.User, "new user")
+	}
+	if *pwd.Password != "new password" {
+		t.Errorf("got: %v, expected: %v", *pwd.Password, "new password")
+	}
+	if *pwd.Mail != "new mail" {
+		t.Errorf("got: %v, expected: %v", *pwd.Mail, "new mail")
+	}
+	if *pwd.Note != "new note" {
+		t.Errorf("got: %v, expected: %v", *pwd.Note, "new note")
+	}
+}
