f11465d8340889f76382e37e60dead88ce6883b6
kengo-k <31839234+kengo-k@users.noreply.github.com>
2022-12-01 23:05:49 +0900

    #25: 変更ない場合publishしない (#26)

--- src/repo/repo.go
+++ src/repo/repo.go
@@ -17,19 +17,24 @@ func NewRepository(database *model.Database) *Repository {
 	return r
 }
 
-func (repo *Repository) GetSortedCategories() []*model.Category {
-	ret := []*model.Category{}
-	for _, cat := range repo.database.CategoryMap {
-		ret = append(ret, cat)
-	}
-	sort.SliceStable(ret, func(i, j int) bool {
-		a := ret[i]
-		b := ret[j]
-		return a.Order < b.Order
-	})
-	return ret
+func (repo *Repository) GetNextPasswordId() int {
+	return repo.database.GetNextPasswordId()
 }
 
+func (repo *Repository) IsDirty() bool {
+	return repo.database.IsDirty()
+}
+
+func (repo *Repository) SetClean() {
+	repo.database.SetDirty(false)
+}
+
+// get single password by id
+func (repo *Repository) GetPassword(id int) *model.Password {
+	return repo.database.PasswordMap[id]
+}
+
+// find passwords by search condition
 func (repo *Repository) FindPasswords() []*model.Password {
 	ret := []*model.Password{}
 	sortedCats := repo.GetSortedCategories()
@@ -45,18 +50,7 @@ func (repo *Repository) FindPasswords() []*model.Password {
 	return ret
 }
 
-func (repo *Repository) GetPassword(id int) *model.Password {
-	return repo.database.PasswordMap[id]
-}
-
-func (repo *Repository) GetNextPasswordId() int {
-	return repo.database.GetNextPasswordId()
-}
-
-func (repo *Repository) GetCategories() map[string]*model.Category {
-	return repo.database.CategoryMap
-}
-
+// save password into database
 func (repo *Repository) SavePassword(p *model.Password) {
 	if _, ok := repo.database.PasswordMap[p.ID]; !ok {
 		pwds := repo.database.CategorizedPasswords[p.Category.ID]
@@ -64,22 +58,50 @@ func (repo *Repository) SavePassword(p *model.Password) {
 		repo.database.CategorizedPasswords[p.Category.ID] = pwds
 	}
 	repo.database.PasswordMap[p.ID] = p
+	repo.database.SetDirty(true)
 }
 
+// delete password from database
 func (repo *Repository) DeletePassword(p *model.Password) {
 	delete(repo.database.PasswordMap, p.ID)
+	repo.database.SetDirty(true)
 }
 
-func (repo *Repository) SaveCategory(cat *model.Category) {
-	repo.database.CategoryMap[cat.ID] = cat
-}
-
+// get single category by id
 func (repo *Repository) GetCategory(id string) *model.Category {
 	return repo.database.CategoryMap[id]
 }
 
+// get all categories
+func (repo *Repository) GetCategories() map[string]*model.Category {
+	return repo.database.CategoryMap
+}
+
+// get sorted all categories
+func (repo *Repository) GetSortedCategories() []*model.Category {
+	ret := []*model.Category{}
+	for _, cat := range repo.database.CategoryMap {
+		ret = append(ret, cat)
+	}
+	sort.SliceStable(ret, func(i, j int) bool {
+		a := ret[i]
+		b := ret[j]
+		return a.Order < b.Order
+	})
+	return ret
+}
+
+// save category into database
+func (repo *Repository) SaveCategory(cat *model.Category) {
+	repo.database.CategoryMap[cat.ID] = cat
+	repo.database.SetDirty(true)
+}
+
+// delete category from database
 func (repo *Repository) DeleteCategory(cat *model.Category) {
+	// TODO カテゴリ配下のパスワードを一括で消すorカテゴリ配下にパスワードが存在する場合はエラーにする
 	delete(repo.database.CategoryMap, cat.Name)
+	repo.database.SetDirty(true)
 }
 
 func (repo *Repository) Serialize() [][]*model.Password {
